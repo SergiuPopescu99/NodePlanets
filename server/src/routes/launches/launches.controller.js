6f2cadf3c2f16c2f1f6379c49ab35918a4506f55
SergiuPopescu99 <sergiopop25@yahoo.com>
2023-04-11 17:52:10 +0300

    added validation for post method

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -5,7 +5,21 @@ function httpGetAllLaunches(req, res) {
 }
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
+
+    if (!launch.mission || !launch.rocket || !launch.launchDate
+        || !launch.destination) {
+        return res.status(400).json({
+            error: "Missing required lunch properties!",
+        });
+    }
+
     launch.launchDate = new Date(launch.launchDate);
+    //An implicit function will be called on launchDate which will transform it in Unix timestamp
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: "Invalid launch date!",
+        });
+    }
     addNewLaunch(launch);
     return res.status(201).json(launch); //return so there is one response per controller
 
