e2f3d9f248f6ec28755fbc0a2dccfabb5d5f60d5
SergiuPopescu99 <sergiopop25@yahoo.com>
2023-04-12 21:07:38 +0300

    added testing using JEST and supertest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const app = require('../../app');
+describe('Test GET /launches', () => {
+    test('It should respond with 200 success', async () => {
+        const response = await request(app)
+            .get('/launches')
+            .expect('Content-Type', /json/)
+            .expect(200);
+        // expect(response.statusCode).toBe(200);
+    })
+});
+
+describe('TEST POST /launches', () => {
+    const completeLaunchData = {
+        mission: 'RSA-7',
+        rocket: 'ROSA 1801-D',
+        target: 'Keplar-186 f',
+        launchDate: 'January 4,2028'
+    };
+    const launchDataWithoutDate = {
+        mission: 'RSA-7',
+        rocket: 'ROSA 1801-D',
+        target: 'Keplar-186 f',
+
+    }
+
+    test('It should respond with 201 created success', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(completeLaunchData)
+            .expect('Content-Type', /json/)
+            .expect(201);
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate = new Date(response.body.launchDate).valueOf();
+        expect(responseDate).toBe(requestDate);
+        expect(response.body).toMatchObject(launchDataWithoutDate)
+    })
+    test('It should  catch missing requried properties', () => { })
+    test('It should catch invalid dates', () => { })
+})
